1a470293ac6a4bcc34ba0c9cbd511d7dceb5cc0d
CBTE Account <cbte@ac.sdmesa.net>
2020-02-10 10:32:24 -0800

    1c

--- Assets/Course Library/Scripts/PlayerMovement.cs
+++ Assets/Course Library/Scripts/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float Vspeed;
+    public float Hspeed;
+       
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward);
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * Vspeed * verticalInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * Hspeed * horizontalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * Hspeed * horizontalInput);
     }
 }
